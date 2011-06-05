c234eb49fa32aa37f5106a89731622a0c3529a66
Laurent Bossavit <laurent@BigMac.local>
2011-06-05 15:32:55 +0200

    Fix failing timestampt test

--- test/unit/test_annuaire.rb
+++ test/unit/test_annuaire.rb
@@ -21,11 +21,11 @@ class DirectoryTest < Test::Unit::TestCase
 
   def test__registration
     Timecop.freeze(Time.local(2011, 6, 3, 12, 0, 0))
-    person = app.new.register(profile_for "Laurent Bossavit")
+    person = app.new!.register(profile_for "Laurent Bossavit")
     assert_equal "p4sWv6Bm4e", person.id
     assert_equal "Laurent", person.first_name
     assert_equal "Bossavit", person.last_name
-    assert_equal Time.local(2011, 6, 3, 12, 0, 0), person.since
+    assert_equal Time.local(2011, 6, 3, 12, 0, 0), person.since.to_time
   end
 
 end
